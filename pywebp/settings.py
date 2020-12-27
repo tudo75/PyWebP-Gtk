9064b34263406827e8e7ec2ffd4b30f04b9fc73d
tudo75 <tudo75@hotmail.com>
2020-12-27 15:14:29 +0100

    Refactoring of the SettingStorage class that now handle settings via GLib.KeyFile

--- pywebp/settings.py
+++ pywebp/settings.py
@@ -1,151 +1,68 @@
+#
+#  MIT License
+#
+#  Copyright (c) 2019-2020 Nicola Tudino aka tudo75
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in all
+#  copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#  SOFTWARE.
+#
+#
 
-from gi.repository import GObject
-from pywebp.helpers import get_boolean, get_float, get_int
+import os
 
+from gi.repository import GObject, GLib
+from pywebp.helpers import error_message
 
-class Setting(object):
-    """Stores a setting and its attributes.
+DATA_DIR = os.path.join(GLib.get_user_data_dir(), 'pywebp_gtk')
+CONFIG_FILE = os.path.join(DATA_DIR, 'pywebp_gtk.ini')
+SETTINGS_GROUP_NAME = "Settings"
+COPYRIGHT = """
 
-    This class should not be used directly. Instead it is used as BaseClass for
-    different types of settings.
+MIT License
 
-    Attributes:
-        name: Name of the setting as string.
+Copyright (c) 2019-2021 Nicola Tudino aka tudo75
 
-        _datatype: Python type of the setting.
-        _default_value: Default value of the setting stored in its python type.
-        _value: Value of the setting stored in its python type.
-    """
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-    def __init__(self, name, default_value):
-        """Initialize attributes with default values.
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
-        Args:
-            name: Name of the setting to initialize.
-            default_value: Default value of the setting to start with.
-        """
-        self.name = name
-        self._default_value = default_value
-        self._value = default_value
-        self._n = 0
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 
-    def get_default(self):
-        return self._default_value
-
-    def get_value(self):
-        return self._value
-
-    def is_default(self):
-        return self._value == self._default_value
-
-    def set_to_default(self):
-        self._value = self._default_value
-
-
-class BoolSetting(Setting):
-    """Stores a boolean setting."""
-
-    def override(self, new_value):
-        self._value = get_boolean(new_value)
-
-    def toggle(self):
-        self._value = not self._value
-
-
-class IntSetting(Setting):
-    """Stores an integer setting."""
-
-    def override(self, new_value):
-        self._value = get_int(new_value)
-
-    def add(self, value, multiplier):
-        """Add a value to the currently stored integer.
-
-        Args:
-            value: The integer value to add.
-            multiplier: Integer to multiply value with.
-        """
-        value = get_int(value, allow_sign=True)
-        multiplier = get_int(multiplier, allow_sign=False)
-        self._value += value * multiplier
-
-
-class FloatSetting(Setting):
-    """Stores a float setting."""
-
-    def override(self, new_value):
-        self._value = get_float(new_value)
-
-    def add(self, value, multiplier):
-        """Add a value to the currently stored float.
-
-        Args:
-            value: The float value to add.
-            multiplier: Float to multiply value with.
-        """
-        value = get_float(value, allow_sign=True)
-        multiplier = get_float(multiplier, allow_sign=False)
-        self._value += value * multiplier
-
-
-class ThumbSizeSetting(Setting):
-    """Stores a thumbnail size setting.
-
-    This setting is stored as a python tuple with two equal integer values. The
-    integer value must be one of 64, 128, 256, 512.
-    """
-
-    def override(self, new_value):
-        """Override the setting with a new thumbnail size.
-
-        Args:
-            new_value: String in the form of "(val, val)" for the new value.
-        """
-        new_value = new_value.lstrip("(").rstrip(")")
-        int_tuple = new_value.split(",")
-        if len(int_tuple) != 2:
-            error = "Tuple must be of the form (val, val)"
-            raise ValueError(error)
-        int_tuple = (get_int(int_tuple[0]), get_int(int_tuple[1]))
-        if int_tuple[0] != int_tuple[1]:
-            error = "Thumbnail width and height must be equal"
-            raise ValueError(error)
-        if int_tuple[0] not in [64, 128, 256, 512]:
-            error = "Thumbnail size must be one of 64, 128, 256 and 512"
-            raise ValueError(error)
-        self._value = tuple(int_tuple)
-
-
-class GeometrySetting(Setting):
-    """Stores a geometry setting.
-
-    This setting is stored as python tuple of the form (width, height) and can
-    be set from a string in the form "WIDTHxHEIGHT".
-    """
-
-    def override(self, new_value):
-        """Override the setting with a new geometry.
-
-        Args:
-            new_value: String in the form of "WIDTHxHEIGHT" for the new value.
-        """
-        geometry = new_value.split("x")
-        if len(geometry) != 2:
-            error = "Geometry must be of the form WIDTHxHEIGHT"
-            raise ValueError(error)
-        self._value = (get_int(geometry[0]), get_int(geometry[1]))
+"""
 
 
 class SettingStorage(GObject.Object):
-    """Stores all settings for vimiv.
+    """Stores all settings.
 
-    Settings can be accessed via SettingStorage["setting_name"] and changed by
-    calling SettingStorage.override("setting_name", new_value).
-
-    Attributes:
-        _aliases: Dictionary containing all aliases.
-        _settings: List of all Setting objects. Should not be accessed directly.
-        _n: Integer for iterator.
+    Settings can be accessed via SettingStorage.get_type("key") and changed by
+    calling SettingStorage.set_type("key", value), where type is the type of data(boolean,integer,etc).
 
     Signals:
         changed: Emitted when a setting was changed.
@@ -153,83 +70,178 @@ class SettingStorage(GObject.Object):
 
     def __init__(self):
         super(SettingStorage, self).__init__()
-        self._aliases = {}
-        self._settings = [
-            BoolSetting("darkmode", True),
-            ThumbSizeSetting("default_thumbsize", (128, 128)),
-            GeometrySetting("geometry", (800, 400))
-        ]
-        self._n = 0
+        self._init_from_keyfile()
 
-    def override(self, name, new_value=None):
-        """Override a setting in the storage.
+    @staticmethod
+    def _init_from_keyfile():
+        """Initialize te key file in DATA_DIR folder with CONFIG_FILE path and create default values for keys
+        """
+        _keyfile = GLib.KeyFile()
+        # check if DATA_DIR exist and eventually create it
+        if not os.path.exists(DATA_DIR):
+            try:
+                os.makedirs(DATA_DIR)
+            except OSError as error:
+                print(error)
+                print("Creation of the directory %s failed" % DATA_DIR)
+            else:
+                print("Successfully created the directory %s " % DATA_DIR)
+
+        # check if CONFIG_FILE exist and eventually create it
+        if os.path.exists(CONFIG_FILE):
+            _keyfile.load_from_file(CONFIG_FILE, GLib.KeyFileFlags.KEEP_COMMENTS)
+            print("CONFIG_FILE loaded")
+        else:
+            _keyfile = GLib.KeyFile.new()
+            _keyfile.set_comment(None, None, COPYRIGHT)
+            print("no CONFIG_FILE, new keyfile created")
+
+        # check if SETTINGS_GROUP_NAME settings group exist and eventually create and initialize it
+        if not _keyfile.has_group(SETTINGS_GROUP_NAME):
+            _keyfile.set_boolean(SETTINGS_GROUP_NAME, "darkmode", True)
+            _keyfile.set_integer_list(SETTINGS_GROUP_NAME, "default_thumbsize", [128, 128])
+            _keyfile.set_integer_list(SETTINGS_GROUP_NAME, "geometry", [800, 400])
+
+        _keyfile.save_to_file(CONFIG_FILE)
+
+    @staticmethod
+    def _get_keyfile():
+        """Load keyfile from CONFIG_FILE
+
+        Returns:
+            keyfile containing settings
+        """
+        _keyfile = GLib.KeyFile()
+        try:
+            _keyfile.load_from_file(CONFIG_FILE, GLib.KeyFileFlags.KEEP_COMMENTS)
+        except GLib.Error as error:
+            error_message(error)
+        finally:
+            return _keyfile
+
+    def get_boolean(self, key) -> bool:
+        """Return boolean value of the key setting
 
         Args:
-            name: Name of the setting to override.
-            new_value: Value to override the setting with. If None use default.
+            key: the name of the setting containing the boolean value to be retrieved
+
+        Returns:
+            the value associated with the key as a boolean, or False if the key was not found or could not be parsed.
         """
-        setting = self[name]
-        if new_value is None:
-            setting.set_to_default()
-        else:
-            setting.override(new_value)
-        self.emit("changed", name)
+        keyfile = self._get_keyfile()
+        try:
+            result = keyfile.get_boolean(SETTINGS_GROUP_NAME, key)
+        except GLib.Error as error:
+            error_message(error.message)
+            result = False
 
-    def toggle(self, name):
-        """Toggle a setting in the storage.
+        return result
 
-        If the setting is not a boolean, an exception is raised.
+    def set_boolean(self, key, value):
+        """ Associates a new boolean value with key. If key cannot be found then it is created.
 
         Args:
-            name: Name of the setting to toggle.
+            key: key name
+            value: True or False
+
+        Return:
+            True if value is set, False otherwise
         """
-        if isinstance(self[name], BoolSetting):
-            self[name].toggle()
-            self.emit("changed", name)
-        else:
-            error = "Cannot toggle non boolean setting '%s'" % (name)
-            raise Exception(error)
+        keyfile = self._get_keyfile()
+        result = False
+        if isinstance(value, bool):
+            try:
+                keyfile.set_boolean(SETTINGS_GROUP_NAME, key, value)
+                keyfile.save_to_file(CONFIG_FILE)
+            except GLib.Error as error:
+                error_message(error.message)
+            finally:
+                result = True
 
-    def add_to(self, name, value, multiplier):
-        """Add a number to a setting in the storage.
+        return result
 
-        If the setting is not a number, an exception is raised.
+    def get_integer(self, key) -> int:
+        """Return integer value of the key setting
 
         Args:
-            name: Name of the setting to change.
-            value: Value to add.
-            multiplier: Multiply value by this. Used for num_str.
+            key: the name of the setting containing the integer value to be retrieved
+
+        Returns:
+            the value associated with the key as an integer, or 0 if the key was not found or could not be parsed.
         """
-        if isinstance(self[name], (IntSetting, FloatSetting)):
-            self[name].add(value, multiplier)
-            self.emit("changed", name)
-        else:
-            error = "Cannot add to non number setting '%s'" % (name)
-            raise Exception(error)
+        keyfile = self._get_keyfile()
+        try:
+            result = keyfile.get_integer(SETTINGS_GROUP_NAME, key)
+        except GLib.Error as error:
+            error_message(error.message)
+            result = 0
 
-    def reset(self):
-        """Reset all settings to their default value."""
-        for setting in self:
-            setting.set_to_default()
+        return result
 
-    def __getitem__(self, name):
-        """Receive a setting via its name."""
-        for setting in self._settings:
-            if setting.name == name:
-                return setting
-        raise Exception("No setting called '%s'" % (name))
+    def set_integer(self, key, value):
+        """ Associates a new integer value with key. If key cannot be found then it is created.
 
-    def __iter__(self):
-        self._n = 0
-        return self
+        Args:
+            key: key name
+            value: an integer value
 
-    def __next__(self):
-        """Iterate over settings."""
-        if (self._n - 1) < len(self._settings) - 1:
-            self._n += 1
-            return self._settings[self._n - 1]
-        else:
-            raise StopIteration
+        Return:
+            True if value is set, False otherwise
+        """
+        keyfile = self._get_keyfile()
+        result = False
+        if isinstance(value, int):
+            try:
+                keyfile.set_integer(SETTINGS_GROUP_NAME, key, value)
+                keyfile.save_to_file(CONFIG_FILE)
+            except GLib.Error as error:
+                error_message(error.message)
+            finally:
+                result = True
+
+        return result
+
+    def get_integer_list(self, key) -> [int]:
+        """Return a list of integer values of the key setting
+
+        Args:
+            key: the name of the setting containing the list of integer values to be retrieved
+
+        Returns:
+            the values associated with the key as a list of integers, or an empty list [] if the key was not found or
+            could not be parsed. The returned list of integers should be freed with GLib.free() when no longer needed.
+        """
+        keyfile = self._get_keyfile()
+        try:
+            result = keyfile.get_integer_list(SETTINGS_GROUP_NAME, key)
+        except GLib.Error as error:
+            error_message(error.message)
+            result = [0]
+
+        return result
+
+    def set_integer_list(self, key, my_list):
+        """ Associates a list of integer values with key. If key cannot be found then it is created.
+
+        Args:
+            key: key name
+            my_list: an array of integer values
+
+        Return:
+            True if value is set, False otherwise
+        """
+        keyfile = self._get_keyfile()
+        result = False
+        if all(isinstance(item, int) for item in my_list):
+            try:
+                keyfile.set_integer_list(SETTINGS_GROUP_NAME, key, my_list)
+                keyfile.save_to_file(CONFIG_FILE)
+            except GLib.Error as error:
+                error_message(error.message)
+            finally:
+                result = True
+
+        return result
 
 
 # Initiate signals for the SettingsStorage
